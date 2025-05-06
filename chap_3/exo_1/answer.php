30381abcbd026b24da30b109ccccdc480cda2dae
Yandelf00 <zakaria.afyf00@gmail.com>
2025-05-07 00:28:46 +0100

    chapter 3 in progress

---
+++ chap_3/exo_1/answer.php
@@ -0,0 +1,10 @@
+<?php
+
+$x = 6;
+
+if ($x % 3 == 0 AND $x % 5 == 0) {
+	echo "divisible par trois et cinq";
+} 
+else {
+	echo "non divisible par trois et cinq";
+}
---
+++ chap_3/exo_2/answer.php
@@ -0,0 +1,13 @@
+<?php
+$age = 22;
+$sexe = "F";
+
+if(21 <= $age AND $age <= 40 AND $sexe == "F")
+{
+	echo "Vous avez les critères\n";
+}
+else 
+{
+	echo "Vous n'aves pas les critères\n";
+}
+
